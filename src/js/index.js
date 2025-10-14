bc56ed0b8f65fbca856737750a295e9c832fb1fe
jmrgon <jmrobledogonzalez@gmail.com>
2025-10-14 22:00:32 +0200

    feat:trabajo en casa

---
+++ src/js/index.js
@@ -0,0 +1,9 @@
+document.querySelectorAll('.recuadro').forEach(recuadro => {
+            recuadro.addEventListener('click', () => {
+                window.location.href = 'https://github.com/Jmrgon';
+            });
+        });
+
+document.getElementById('boton-redes').addEventListener('click', () => {
+        window.location.href = 'redes-sociales.html';
+    });
